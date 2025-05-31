46da96eef23ba0381b2abbd9a6cca9dd59f92738
Michael Paul Killian <rad@killian.email>
2025-05-31 13:00:00 +0200

    elm-pages.config.mjs aktualisieren

--- elm-pages.config.mjs
+++ elm-pages.config.mjs
@@ -4,9 +4,9 @@ export default {
   vite: defineConfig({}),
   headTagsTemplate(context) {
     return `
-<link rel="stylesheet" href="/style.css" />
-<link rel="icon" href="/media/logo.svg">
-<link rel="mask-icon" href="/media/logo.svg" color="#000000">
+<link rel="stylesheet" href="style.css" />
+<link rel="icon" href="media/logo.svg">
+<link rel="mask-icon" href="media/logo.svg" color="#000000">
 <link rel="apple-touch-icon" href="logo-180.png">
 <meta name="generator" content="elm-pages v${context.cliVersion}" />
 <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
